03b2d14a133d67b74541c13a16ca45eba75ec9cc
Sinkyo <yhko1988@fastlane.kr>
2023-04-30 15:29:03 +0900

    feat : nav 완성

--- src/components/web/FirstWebSlide.tsx
+++ src/components/web/FirstWebSlide.tsx
@@ -1,9 +1,24 @@
+import Image from "next/image";
 import React from "react";
 
 export const FirstWebSlide = () => {
   return (
-    <div className="font-futura font-[500] relative h-screen w-screen  text-[80px] text-white">
-      4MHz
-    </div>
+    <section className="relative h-screen w-screen font-futura text-[80px]  font-[500] text-white">
+      <nav className="flex-center mt-[64px] justify-between px-[162px]">
+        <div>
+          <Image
+            src="/img/therfect_logo.png"
+            height={33}
+            width={160}
+            alt="therfect_logo"
+          />
+        </div>
+        <div className="flex items-center gap-[10px]">
+          <span className="text-[20px] tracking-[0.02em]">KR</span>
+          <span className="text-[20px] opacity-40">|</span>
+          <span className="text-[20px] tracking-[0.02em] opacity-40">EN</span>
+        </div>
+      </nav>
+    </section>
   );
 };

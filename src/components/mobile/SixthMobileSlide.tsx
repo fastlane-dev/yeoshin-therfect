060b95266d94fdc08169889cccc6882789478e44
Sinkyo <yhko1988@fastlane.kr>
2023-05-16 13:57:48 +0900

    fix : 기기에 내장되어있는 bottom nav bar의 크기를 감지
    - 'env' + 'safe-area' css property를 이용함

--- src/components/mobile/SixthMobileSlide.tsx
+++ src/components/mobile/SixthMobileSlide.tsx
@@ -6,7 +6,10 @@ import { InviewComponent } from "../common/InviewComponent";
 
 export const SixthMobileSlide = () => {
   return (
-    <section className="relative flex h-screen w-screen flex-col justify-between overflow-hidden bg-black px-[47px] pb-[83px] pt-[25px] font-pretendard font-[400] text-white">
+    <section
+      className="relative flex h-screen w-screen flex-col justify-between overflow-hidden bg-black px-[47px] pt-[70px] font-pretendard font-[400] text-white"
+      style={{ paddingBottom: "calc(10px + env(safe-area-inset-bottom))" }}
+    >
       <div className="absolute  left-[50%] top-[50%] h-[5.3vh] w-[79vw] -translate-x-1/2 -translate-y-[350%]">
         <InviewComponent
           threshold={0}

c4c3ac3fecb7551a474305b16794a4d04ac8481b
Sinkyo <yhko1988@fastlane.kr>
2023-05-23 10:08:49 +0900

    feat : 3/4번째 슬라이드 scroll로 구현
    - 4번째 슬라이드 하단이 짤리는 것을 위해 높이를 확보

--- src/components/mobile/Mobile.tsx
+++ src/components/mobile/Mobile.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 
-import { Mousewheel } from "swiper";
+import { FreeMode, Mousewheel, Scrollbar } from "swiper";
 
 import { FirstMobileSlide } from "./FirstMobileSlide";
 import { SecondMobileSlide } from "./SecondMobileSlide";
@@ -16,7 +16,59 @@ import { NinethMobileSlide } from "./NinethMobileSlide";
 
 const Mobile = () => {
   const [innerHeight, setInnerHeight] = useState(0);
+  const [scrollSectionHeight, setScrollSectionHeight] = useState(0);
 
+  useEffect(() => {
+    const viewWidth = window.innerHeight;
+    if (viewWidth <= 500) {
+      setScrollSectionHeight(800);
+    } else if (viewWidth > 500 && viewWidth <= 600) {
+      setScrollSectionHeight(1000);
+    } else if (viewWidth > 600 && viewWidth <= 700) {
+      setScrollSectionHeight(1200);
+    } else if (viewWidth > 700 && viewWidth <= 800) {
+      setScrollSectionHeight(1400);
+    } else if (viewWidth > 800 && viewWidth <= 900) {
+      setScrollSectionHeight(1600);
+    } else if (viewWidth > 900) {
+      setScrollSectionHeight(1800);
+    }
+    window.addEventListener("resize", () => {
+      const viewHeight = window.innerHeight;
+      if (viewHeight <= 500) {
+        setScrollSectionHeight(800);
+      } else if (viewHeight > 500 && viewHeight <= 600) {
+        setScrollSectionHeight(1000);
+      } else if (viewHeight > 600 && viewHeight <= 700) {
+        setScrollSectionHeight(1200);
+      } else if (viewHeight > 700 && viewHeight <= 800) {
+        setScrollSectionHeight(1400);
+      } else if (viewHeight > 800 && viewHeight <= 900) {
+        setScrollSectionHeight(1600);
+      } else if (viewHeight > 900) {
+        setScrollSectionHeight(1800);
+      }
+    });
+
+    return () => {
+      window.removeEventListener("resize", () => {
+        const viewHeight = window.innerHeight;
+        if (viewHeight <= 500) {
+          setScrollSectionHeight(800);
+        } else if (viewHeight > 500 && viewHeight <= 600) {
+          setScrollSectionHeight(1000);
+        } else if (viewHeight > 600 && viewHeight <= 700) {
+          setScrollSectionHeight(1200);
+        } else if (viewHeight > 700 && viewHeight <= 800) {
+          setScrollSectionHeight(1400);
+        } else if (viewHeight > 800 && viewHeight <= 900) {
+          setScrollSectionHeight(1600);
+        } else if (viewHeight > 900) {
+          setScrollSectionHeight(1800);
+        }
+      });
+    };
+  }, [window.innerWidth]);
   useEffect(() => {
     setInnerHeight(window.innerHeight);
   }, []);
@@ -40,10 +92,22 @@ const Mobile = () => {
         <SecondMobileSlide innerHeight={innerHeight} />
       </SwiperSlide>
       <SwiperSlide>
-        <ThirdMobileSlide innerHeight={innerHeight} />
-      </SwiperSlide>
-      <SwiperSlide>
-        <FourthMobileSlide innerHeight={innerHeight} />
+        <Swiper
+          className="bg-[#F9F9F9]"
+          direction={"vertical"}
+          freeMode={true}
+          scrollbar={true}
+          mousewheel={true}
+          modules={[FreeMode, Scrollbar]}
+          height={innerHeight + 100}
+        >
+          <SwiperSlide className="flex-center">
+            <ThirdMobileSlide innerHeight={innerHeight} />
+          </SwiperSlide>
+          <SwiperSlide className="flex-center bg-[#F9F9F9]">
+            <FourthMobileSlide innerHeight={innerHeight} />
+          </SwiperSlide>
+        </Swiper>
       </SwiperSlide>
       <SwiperSlide>
         <FifthMobileSlide innerHeight={innerHeight} />

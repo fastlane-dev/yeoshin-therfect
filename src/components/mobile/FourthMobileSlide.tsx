c31d794c20e6cd8fcbe888ebc7df62f2bb7d0bbf
Sinkyo <yhko1988@fastlane.kr>
2023-05-23 10:09:43 +0900

    fix : 노즐 리팩토링 및 반응형 재조정

--- src/components/mobile/FourthMobileSlide.tsx
+++ src/components/mobile/FourthMobileSlide.tsx
@@ -1,39 +1,22 @@
 import React from "react";
 import { InviewComponent } from "../common/InviewComponent";
 
-type NozzleType = {
-  imageMb?: string;
-  imageWidth: string;
-  imageHeight: string;
-  titleSize?: string;
-  subTitleSize?: string;
-  subTitleHeight?: string;
-  title: string;
-  subTitle: string;
-  imgSrc: string;
-};
+type NozzleType = Record<
+  "title" | "subTitle" | "imgSrc" | "imgClass" | "titleClass" | "subTitleClass",
+  string
+>;
 
 const Nozzle = ({
-  imageMb = "28px",
-  imageWidth,
-  imageHeight,
-  titleSize = "18px",
-  subTitleSize = "13px",
-  subTitleHeight = "20px",
+  imgSrc,
   title,
   subTitle,
-  imgSrc,
+  imgClass,
+  titleClass,
+  subTitleClass,
 }: NozzleType) => {
   return (
     <div className="flex-center-column">
-      <div
-        className="relative"
-        style={{
-          marginBottom: imageMb,
-          width: imageWidth,
-          height: imageHeight,
-        }}
-      >
+      <div className={imgClass}>
         <img
           sizes="(max-width: 768px) 100dvw,
           (max-width: 1200px) 50dvw,
@@ -43,18 +26,11 @@ const Nozzle = ({
           alt="first_nozzle"
         />
       </div>
-      <InviewComponent animationClassName="animate-smoothUpFirst">
-        <div className="mb-[0.8dvh]" style={{ fontSize: titleSize }}>
-          {title}
-        </div>
+      <InviewComponent animationClassName={`animate-smoothUpFirst`}>
+        <div className={titleClass}>{title}</div>
       </InviewComponent>
       <InviewComponent animationClassName={"animate-smoothUpSecond"}>
-        <div
-          className="text-[4dvw] font-[600] leading-[3dvh]"
-          style={{ lineHeight: subTitleHeight, fontSize: subTitleSize }}
-        >
-          {subTitle}
-        </div>
+        <div className={subTitleClass}>{subTitle}</div>
       </InviewComponent>
     </div>
   );
@@ -62,276 +38,60 @@ const Nozzle = ({
 
 export const FourthMobileSlide = ({ innerHeight }: { innerHeight: number }) => {
   return (
-    <section className="flex-center relative h-screen w-screen overflow-hidden bg-[#F9F9F9] pt-[9dvh] text-center font-pretendard font-[700]">
-      <div className="flex flex-col">
-        <div className="mb-[34px] flex justify-center gap-[60px]">
-          <div className="flex-center-column">
-            <div className="relative  mb-[20px] w-[31dvw]">
-              <img
-                sizes="(max-width: 768px) 100dvw,
-(max-width: 1200px) 50dvw,
-33dvw"
-                className="h-full w-full object-contain"
-                src="/img/first_nozzle.png"
-                alt="first_nozzle"
-              />
-            </div>
-            <InviewComponent animationClassName="animate-smoothUpFirst">
-              <div className="mb-[0.8dvh] text-[5dvw]">써말아이</div>
-            </InviewComponent>
-            <InviewComponent animationClassName={"animate-smoothUpSecond"}>
-              <div className="text-[4dvw] font-[600] leading-[3dvh]">
-                {"팔자, 눈가주름을\n타이트닝"}
-              </div>
-            </InviewComponent>
-          </div>
-          <div className="flex-center-column">
-            <div className="relative  mb-[20px] w-[31dvw]">
-              <img
-                sizes="(max-width: 768px) 100dvw,
-(max-width: 1200px) 50dvw,
-33dvw"
-                className="h-full w-full object-contain"
-                src="/img/fourth_nozzle.png"
-                alt="first_nozzle"
-              />
-            </div>
-            <InviewComponent animationClassName={"animate-smoothUpFirst"}>
-              <div className="mb-[0.8dvh] text-[5dvw]">프락셔널 64P</div>
-            </InviewComponent>
-            <InviewComponent animationClassName={"animate-smoothUpSecond"}>
-              <div className="text-[4dvw] font-[600] leading-[3dvh]">
-                {"비교적 깊은 모공과\n흉터에 시술"}
-              </div>
-            </InviewComponent>
-          </div>
+    <section
+      style={{ height: innerHeight - 10 }}
+      className="flex-center relative top-[-12dvh] w-screen bg-[#F9F9F9] text-center font-pretendard font-[700]"
+    >
+      <div className="h-screen w-screen px-[31px]">
+        <div className="mb-[34px] flex justify-center gap-[16dvw]">
+          <Nozzle
+            imgClass="relative  mb-[20px] w-[31dvw] msm:w-[21dvw] mmd:w-[16dvw]"
+            titleClass="mb-[0.8dvh] text-[5dvw] msm:text-[4dvw] mmd:text-[3dvw]"
+            subTitleClass="text-[4dvw] font-[600] leading-[3dvh] msm:text-[3dvw] mmd:text-[2dvw]"
+            imgSrc="/img/first_nozzle.png"
+            title="써말아이"
+            subTitle={"팔자, 눈가주름을\n타이트닝"}
+          />
+          <Nozzle
+            imgClass="relative  mb-[20px] w-[31dvw] msm:w-[21dvw] mmd:w-[16dvw]"
+            titleClass="mb-[0.8dvh] text-[5dvw] msm:text-[4dvw] mmd:text-[3dvw]"
+            subTitleClass="text-[4dvw] font-[600] leading-[3dvh] msm:text-[3dvw] mmd:text-[2dvw]"
+            imgSrc="/img/fourth_nozzle.png"
+            title="프락셔널 64P"
+            subTitle={"비교적 깊은 모공과\n흉터에 시술"}
+          />
         </div>
-        <div className="mb-[34px] flex justify-center gap-[60px]">
-          <div className="flex-center-column">
-            <div className="relative mb-[20px] w-[29dvw]">
-              <img
-                sizes="(max-width: 768px) 100dvw,
-(max-width: 1200px) 50dvw,
-33dvw"
-                className="h-full w-full object-contain"
-                src="/img/second_nozzle.png"
-                alt="second_nozzle"
-              />
-            </div>
-            <InviewComponent animationClassName={"animate-smoothUpFirst"}>
-              <div className="mb-[0.8dvh] text-[5dvw]">써말 러빙</div>
-            </InviewComponent>
-            <InviewComponent animationClassName={"animate-smoothUpSecond"}>
-              <div className="text-[4dvw] font-[600] leading-[3dvh]">
-                {"처짐, 얼굴윤곽을\n자극하여 얼굴선 정리"}
-              </div>
-            </InviewComponent>
-          </div>
-          <div className="flex-center-column">
-            <div className="relative  mb-[20px] w-[31dvw]">
-              <img
-                sizes="(max-width: 768px) 100dvw,
-(max-width: 1200px) 50dvw,
-33dvw"
-                className="h-full w-full object-contain"
-                src="/img/fifth_nozzle.png"
-                alt="first_nozzle"
-              />
-            </div>
-            <InviewComponent animationClassName={"animate-smoothUpFirst"}>
-              <div className="mb-[0.8dvh] text-[5dvw]">프락셔널 100P</div>
-            </InviewComponent>
-            <InviewComponent animationClassName={"animate-smoothUpSecond"}>
-              <div className="text-[4dvw] font-[600] leading-[3dvh]">
-                {"비교적 깊은 모공과\n흉터에 시술"}
-              </div>
-            </InviewComponent>
-          </div>
+        <div className="mb-[34px] flex justify-center gap-[16dvw]">
+          <Nozzle
+            imgClass="relative  mb-[20px] w-[31dvw] msm:w-[21dvw] mmd:w-[16dvw]"
+            titleClass="mb-[0.8dvh] text-[5dvw] msm:text-[4dvw] mmd:text-[3dvw]"
+            subTitleClass="text-[4dvw] font-[600] leading-[3dvh] msm:text-[3dvw] mmd:text-[2dvw]"
+            imgSrc="/img/second_nozzle.png"
+            title="써말 러빙"
+            subTitle={"처짐, 얼굴윤곽을\n자극하여 얼굴선 정리"}
+          />
+          <Nozzle
+            imgClass="relative  mb-[20px] w-[31dvw] msm:w-[21dvw] mmd:w-[16dvw]"
+            titleClass="mb-[0.8dvh] text-[5dvw] msm:text-[4dvw] mmd:text-[3dvw]"
+            subTitleClass="text-[4dvw] font-[600] leading-[3dvh] msm:text-[3dvw] mmd:text-[2dvw]"
+            imgSrc="/img/fifth_nozzle.png"
+            title="프락셔널 100P"
+            subTitle={"비교적 깊은 모공과\n흉터에 시술"}
+          />
         </div>
-        <div className="flex justify-center gap-[60px]">
-          <div className="flex-center-column">
-            <div className="relative mb-[20px] w-[31dvw]">
-              <img
-                sizes="(max-width: 768px) 100dvw,
-(max-width: 1200px) 50dvw,
-33dvw"
-                className="h-full w-full object-contain"
-                src="/img/third_nozzle.png"
-                alt="third_nozzle"
-              />
-            </div>
-            <InviewComponent animationClassName={"animate-smoothUpFirst"}>
-              <div className="mb-[0.8dvh] text-[5dvw]">써말 스탬프</div>
-            </InviewComponent>
-            <InviewComponent animationClassName={"animate-smoothUpSecond"}>
-              <div className="text-[4dvw] font-[600] leading-[3dvh]">
-                {"강력한 고주파\n에너지를 깊게"}
-              </div>
-            </InviewComponent>
-          </div>
-          <div className="w-[31dvw]"></div>
+        <div className="flex justify-center gap-[16dvw]">
+          <Nozzle
+            imgClass="relative  mb-[20px] w-[31dvw] msm:w-[21dvw] mmd:w-[16dvw]"
+            titleClass="mb-[0.8dvh] text-[5dvw] msm:text-[4dvw] mmd:text-[3dvw]"
+            subTitleClass="text-[4dvw] font-[600] leading-[3dvh] msm:text-[3dvw] mmd:text-[2dvw]"
+            imgSrc="/img/third_nozzle.png"
+            title="써말 스탬프"
+            subTitle={"강력한 고주파\n에너지를 깊게"}
+          />
+
+          <div className="relative  mb-[20px] w-[31dvw] msm:w-[21dvw] mmd:w-[16dvw]"></div>
         </div>
       </div>
     </section>
   );
 };
-
-{
-  /*  <div className="flex flex-col">
-        <div className="mb-[34px] flex justify-center gap-[60px]">
-          <div className="flex-center-column">
-            <div className="relative  mb-[20px] w-[31dvw]">
-              <img
-                sizes="(max-width: 768px) 100dvw,
-(max-width: 1200px) 50dvw,
-33dvw"
-                className="h-full w-full object-contain"
-                src="/img/first_nozzle.png"
-                alt="first_nozzle"
-              />
-            </div>
-            <InviewComponent animationClassName="animate-smoothUpFirst">
-              <div className="mb-[0.8dvh] text-[5dvw]">써말아이</div>
-            </InviewComponent>
-            <InviewComponent animationClassName={"animate-smoothUpSecond"}>
-              <div className="text-[4dvw] font-[600] leading-[3dvh]">
-                {"팔자, 눈가주름을\n타이트닝"}
-              </div>
-            </InviewComponent>
-          </div>
-          <div className="flex-center-column">
-            <div className="relative  mb-[20px] w-[31dvw]">
-              <img
-                sizes="(max-width: 768px) 100dvw,
-(max-width: 1200px) 50dvw,
-33dvw"
-                className="h-full w-full object-contain"
-                src="/img/fourth_nozzle.png"
-                alt="first_nozzle"
-              />
-            </div>
-            <InviewComponent animationClassName={"animate-smoothUpFirst"}>
-              <div className="mb-[0.8dvh] text-[5dvw]">프락셔널 64P</div>
-            </InviewComponent>
-            <InviewComponent animationClassName={"animate-smoothUpSecond"}>
-              <div className="text-[4dvw] font-[600] leading-[3dvh]">
-                {"비교적 깊은 모공과\n흉터에 시술"}
-              </div>
-            </InviewComponent>
-          </div>
-        </div>
-        <div className="mb-[34px] flex justify-center gap-[60px]">
-          <div className="flex-center-column">
-            <div className="relative mb-[20px] w-[29dvw]">
-              <img
-                sizes="(max-width: 768px) 100dvw,
-(max-width: 1200px) 50dvw,
-33dvw"
-                className="h-full w-full object-contain"
-                src="/img/second_nozzle.png"
-                alt="second_nozzle"
-              />
-            </div>
-            <InviewComponent animationClassName={"animate-smoothUpFirst"}>
-              <div className="mb-[0.8dvh] text-[5dvw]">써말 러빙</div>
-            </InviewComponent>
-            <InviewComponent animationClassName={"animate-smoothUpSecond"}>
-              <div className="text-[4dvw] font-[600] leading-[3dvh]">
-                {"처짐, 얼굴윤곽을\n자극하여 얼굴선 정리"}
-              </div>
-            </InviewComponent>
-          </div>
-          <div className="flex-center-column">
-            <div className="relative  mb-[20px] w-[31dvw]">
-              <img
-                sizes="(max-width: 768px) 100dvw,
-(max-width: 1200px) 50dvw,
-33dvw"
-                className="h-full w-full object-contain"
-                src="/img/fifth_nozzle.png"
-                alt="first_nozzle"
-              />
-            </div>
-            <InviewComponent animationClassName={"animate-smoothUpFirst"}>
-              <div className="mb-[0.8dvh] text-[5dvw]">프락셔널 100P</div>
-            </InviewComponent>
-            <InviewComponent animationClassName={"animate-smoothUpSecond"}>
-              <div className="text-[4dvw] font-[600] leading-[3dvh]">
-                {"비교적 깊은 모공과\n흉터에 시술"}
-              </div>
-            </InviewComponent>
-          </div>
-        </div>
-        <div className="flex justify-center gap-[60px]">
-          <div className="flex-center-column">
-            <div className="relative mb-[20px] w-[31dvw]">
-              <img
-                sizes="(max-width: 768px) 100dvw,
-(max-width: 1200px) 50dvw,
-33dvw"
-                className="h-full w-full object-contain"
-                src="/img/third_nozzle.png"
-                alt="third_nozzle"
-              />
-            </div>
-            <InviewComponent animationClassName={"animate-smoothUpFirst"}>
-              <div className="mb-[0.8dvh] text-[5dvw]">써말 스탬프</div>
-            </InviewComponent>
-            <InviewComponent animationClassName={"animate-smoothUpSecond"}>
-              <div className="text-[4dvw] font-[600] leading-[3dvh]">
-                {"강력한 고주파\n에너지를 깊게"}
-              </div>
-            </InviewComponent>
-          </div>
-          <div className="w-[31dvw]"></div>
-        </div>
-      </div> */
-}
-
-{
-  /* <div className="h-screen w-screen px-[31px]">
-<div className="mb-[34px] flex justify-center gap-[60px]">
-  <Nozzle
-    imageWidth="115px"
-    imageHeight="104px"
-    imgSrc="/img/first_nozzle.png"
-    title="써말아이"
-    subTitle={"팔자, 눈가주름을\n타이트닝"}
-  />
-  <Nozzle
-    imageWidth="107px"
-    imageHeight="104px"
-    imgSrc="/img/fourth_nozzle.png"
-    title="프락셔널 64P"
-    subTitle={"비교적 깊은 모공과\n흉터에 시술"}
-  />
-</div>
-<div className="mb-[34px] flex justify-center gap-[60px]">
-  <Nozzle
-    imageWidth="107px"
-    imageHeight="105px"
-    imgSrc="/img/second_nozzle.png"
-    title="써말 러빙"
-    subTitle={"처짐, 얼굴윤곽을\n자극하여 얼굴선 정리"}
-  />
-  <Nozzle
-    imageWidth="107px"
-    imageHeight="105px"
-    imgSrc="/img/fifth_nozzle.png"
-    title="프락셔널 100P"
-    subTitle={"비교적 깊은 모공과\n흉터에 시술"}
-  />
-</div>
-<div className="flex justify-center gap-[60px]">
-  <Nozzle
-    imageWidth="115px"
-    imageHeight="113px"
-    imgSrc="/img/third_nozzle.png"
-    title="써말 스탬프"
-    subTitle={"강력한 고주파\n에너지를 깊게"}
-  />
-
-  <div className="w-[115px]"></div>
-</div>
-</div> */
-}
